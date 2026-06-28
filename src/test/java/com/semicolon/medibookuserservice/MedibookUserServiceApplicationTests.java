cc8134b38e92807b2082d29f584a338af4c0ec34
David-282 <afolabidavid94@gmail.com>
2026-06-28 18:17:55 +0100

    feat: project file structure

---
+++ src/test/java/com/semicolon/medibookuserservice/MedibookUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semicolon.medibookuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedibookUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
